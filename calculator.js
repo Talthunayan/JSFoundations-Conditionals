7b4049f6253f208b8306155d87fb86fdc007077f
Talthunayan <78157264+Talthunayan@users.noreply.github.com>
2021-02-02 18:21:00 +0300

    Solved Conditional task.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,40 @@
 // Define the numbers and the operation here
-
+let x=5;
+let y=0;
+let operation="+";
 // Write if-statements here
+
+// +
+if(operation=="+"){
+    let ans=x+y;
+    console.log(x+operation+y+"= "+ans);
+}
+//  -
+else if (operation=="-"){
+    if (x<y){
+        console.log("Answer is Negative:")
+    } else {
+        console.log(" Answer is Posative:")
+    }
+let ans=x-y;
+console.log(x+operation+y+"= "+ans);
+}
+
+// *
+else if (operation=="x"){
+    let ans=x*y;
+    console.log(x+operation+y+"= "+ans);
+
+} 
+// /
+else if(operation=="/"){
+ if(y==0){
+     console.log("Please  change  Y therefore its  an error")
+ }
+ let ans=x/y;
+ console.log(x+operation+y+"= "+ans);
+// 
+}
+   else { 
+    console.log("something wrong");
+}
